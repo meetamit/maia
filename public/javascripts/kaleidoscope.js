8868949ae4b0b1bc88ab052628f2f73f7377a276
Amit Schechter <amit@meetamit.com>
2012-01-21 01:07:24 -0800

    Mod-based, mouse-based kaleidoscope.

--- public/javascripts/kaleidoscope.js
+++ public/javascripts/kaleidoscope.js
@@ -1,6 +1,6 @@
 (function() {
-  maia.CanvasKaleidoscope = CanvasKaleidoscope;
-  function CanvasKaleidoscope($container, alt, numHalfLeaves) {
+  maia.Kaleidoscope = Kaleidoscope;
+  function Kaleidoscope(imgUrl, alt, numHalfLeaves) {
     var canvas = document.createElement('canvas'),
         context = canvas.getContext('2d'),
         img = new Image(),
@@ -13,23 +13,52 @@
     canvas.height = r * 3;
     document.body.appendChild(canvas);
     
-    $(img).bind('load', function() {
-      leafSym = new HalfLeafSymbol(img, alt, ang);
-      hexSym = new HexSymbol(leafSym, alt, r, numHalfLeaves / 2);
-    });
-    img.src = "/images/wooshes_00.png";
-    
-    setInterval(function() {
-      leafSym.update();
-      hexSym.update();
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      
-      for(var i = (j % 2 == 1 ? 0 : -1); i < 3; i++) {
-        for(var j = 0; j < 3; j++) {
-          context.drawImage(hexSym.canvas, (2 * i + j % 2) * alt, j * r * (Math.sin(ang) + 1) - r);
-        }
+    (this.setImgUrl = function(imgUrl) {
+      if(!this.setImgUrl) {
+        $(img).bind('load', function() {
+          //leafSym = new HalfLeafSymbol(new RotatingImg(img), alt, ang);
+          leafSym = new HalfLeafSymbol(new DebrisImg(alt, r * Math.sin(ang), r), alt, ang);
+          hexSym = new HexSymbol(leafSym, alt, r, numHalfLeaves / 2);
+          if($leafCanvasDiv) $leafCanvasDiv.empty().append(leafSym.canvas);
+        });
       }
-    }, 50);
+      img.src = imgUrl;
+    })(imgUrl);
+    
+    var intervalId;
+    var fff=true;
+    (this.play = function() {
+      if(intervalId) return;
+      intervalId = setInterval(function() {
+        leafSym.update();
+        hexSym.update();
+        context.clearRect(0, 0, canvas.width, canvas.height);
+      
+        for(var i = (j % 2 == 1 ? 0 : -1); i < 3; i++) {
+          for(var j = 0; j < 3; j++) {
+            context.drawImage(hexSym.canvas, (2 * i + j % 2) * alt, j * r * (Math.sin(ang) + 1) - r);
+          }
+        }
+      }, 50);
+      if(intervalId == 0) intervalId = '0';
+    })();
+    
+    this.pause = function() {
+      if(intervalId != null) {
+        clearInterval(Number(intervalId));
+        intervalId = null;
+      }
+    };
+    
+    this.playOrPause = function() {
+      if(intervalId) this.pause();
+      else this.play();
+    };
+    
+    var $leafCanvasDiv;
+    this.getLeafCanvas = function() {
+      return $leafCanvasDiv = $leafCanvasDiv || $('<div class="leaf"></div>');
+    };
   }
   
   function HexSymbol(leafSym, alt, r, numLeaves) {
@@ -69,101 +98,94 @@
     // document.body.appendChild(canvas);
     
     context.beginPath();
-    context.moveTo(-2,0);
+    context.moveTo(0,0);
     context.lineTo(w,0);
-    context.lineTo(w,h+2);
+    context.lineTo(w,h);
     context.clip();
 
     var rot = 0;
     (this.update = function() {
       context.save(); 
       context.clearRect(0, 0, w, h);
-      context.translate(img.width/2, 0*img.height/2);
-      context.rotate(rot * Math.PI / 180);
-      context.translate(-img.width/2, -img.height/2);
-      rot -=1;
-      context.drawImage(img, 0, 0);
+      img.drawInto(context);
       context.restore();
     })();
   }
-})();
+  
+  function DebrisImg(w,h,r) {
+    var r1 = 20,
+        r2 = 0;
+    this.drawInto = function (context) {
+      var radgrad = context.createRadialGradient(Kaleidoscope.mod1*w,0,Kaleidoscope.mod2*8,0,0,r);
+      radgrad.addColorStop(0, '#A7D30C');
+      radgrad.addColorStop(Kaleidoscope.mod2*.5 + .5, '#019F62');
+      // radgrad.addColorStop(1, 'rgba(1,159,98,1)');
+      context.fillStyle = radgrad;
+      context.beginPath();
+      context.arc(0,0,r,0,2*Math.PI);
+      context.fill();
 
-(function() {
-  maia.Kaleidoscope = Kaleidoscope;
-  function Kaleidoscope($container, h, numHalfLeaves) {
+      context.fillStyle = 'rgba(0,0,0,.5)';
+      context.lineWidth = 4;
+      context.beginPath();
+      var rr = (Kaleidoscope.mod1*.7 + .3) * w*.7,
+          k = -Kaleidoscope.mod2 * h;
+      context.arc(w/2,h/2+k,rr,0,2*Math.PI);
+      context.lineTo(w/2+r1, h/2+k);
+      context.arc(w/2,h/2+k,Math.max(0,rr-10),0,2*Math.PI,true);
+      context.fill();
 /*
-    for(var i = 0; i < 3; i++) {
-      for(var j = (i == 1 ? -1 : 0); j < 2; j++) {
-        var $hex = $('<div class="hexagon"></div>').appendTo($container),
-            hex = new Hexagon($hex, 12);
       
-        $hex.css({
-          left:i * 302,
-          top: j * 350 + (i % 2) * 175
-        });
-      }
-    }    
+      context.strokeStyle = 'rgba(0,0,0,.5)';
+      context.lineWidth = 10;
+      context.beginPath();
+      context.moveTo(Kaleidoscope.mod1 * w-5,-5);
+      context.lineTo(w+5,(1-Kaleidoscope.mod2)*h+5);
+      context.stroke();
+      context.beginPath();
+      context.moveTo((1-Kaleidoscope.mod1) * w-5,-5);
+      context.lineTo((1-Kaleidoscope.mod2) * w+5,h+5);
+      context.stroke();
 */
-//http://amitair.local:3000/kaleidoscope.html
-    // var w = Math.round(h * 200/350);
-    var w = Math.round( h * Math.tan(2 * Math.PI / numHalfLeaves) );
-    new Hexagon( $('<div class="hexagon"></div>').appendTo($container), numHalfLeaves, w, h );
-
-    $('.leaf').css({
-      '-webkit-transform-origin': w + 'px ' + h + 'px',
-      width: w*2,
-      height: h,
-      left: -w,
-      top: -h
-    });
-    
-    $('.half_leaf').css({
-      // '-webkit-mask':'url("/images/triangle_mask_200x350.png") no-repeat scroll left bottom;',
-      '-webkit-mask':'url("/images/triangle_mask_' + w + 'x' + h + '.png") no-repeat scroll left bottom;',
-      width:w,
-      height:h
-    });
-
-/*    var rules = document.styleSheets[0].cssRules,
-        imgRule = null;
-    for(var i=0; i < rules.length; i++) {
-      if(rules[i].cssText.indexOf('.half_leaf .img') > -1) {
-        imgRule = rules[i];
-      }
-    }
-
-    var tokens = ['-webkit-transform: rotate(', 0, 'deg);'];
-    setInterval(function() {
-      tokens[1] += 1;
-      imgRule.style.cssText = tokens.join('');
-    }, 30);*/
-    
-    var $imgs = $('.hexagon .img'),
-        degs = 0;
-    setInterval(function() {
-      degs += 2;
-      $imgs.css({ '-webkit-transform': 'rotate(' + degs + 'deg)' });
-    }, 30);
+    };
   }
   
-  function Hexagon($container, numHalfLeaves, w, h) {
-    var leafAngle = 360 / numHalfLeaves,
-        $leaf, $half,
-        // imgHtml = '<img class="img" src="/images/wooshes_00.png" width="' + w + '" height="' + w + '">',
-        imgHtml = '<img class="img" src="/images/wooshes_00.png" width="' + w + '" height="' + (h * 300/350) + '">',
-        // imgHtml = '<div class="img" style="width:'+w+'px; height:'+h+'px; font-size:36px;">Amit was here</div>',
-        halfLeafHtml1 = '<div class="half_leaf">' + imgHtml + '</div>',
-        halfLeafHtml2 = '<div class="half_leaf reflected">' + imgHtml + '</div>';
-    for(var i = 0; i < numHalfLeaves; i++) {
-      var zeroMod2 = i % 2 == 0;
-      if(zeroMod2) {
-        $leaf = $('<div class="leaf"></div>').appendTo($container);
-        $leaf.css({
-          '-webkit-transform':'rotate(' + (i * leafAngle) + 'deg)'
-        });
-      }
-      $half = $(zeroMod2 ? halfLeafHtml1 : halfLeafHtml2).appendTo($leaf);
-    }
+  function RotatingImg(imgTag) {
+    var rot = 0;
+    this.drawInto = function (context) {
+      context.translate(imgTag.width/2, 0*imgTag.height/2);
+      context.rotate(rot * Math.PI / 180);
+      context.translate(-imgTag.width/2, -imgTag.height/2);
+      context.drawImage(imgTag, 0, 0);
+      rot -=.25;
+    };
+  }
+  
+  Kaleidoscope.mod1 = Kaleidoscope.mod2 = .5;
+  $(document).ready(function() { 
+    $(document).bind($.browser.touchDevice ? 'touchmove' : 'mousemove', function(e) {
+      var src = $.browser.touchDevice ? e.targetTouches[0] : e;
+      Kaleidoscope.mod1 = Math.min(1, Math.max(0, src.pageX/document.width));
+      Kaleidoscope.mod2 = Math.min(1, Math.max(0, src.pageY/document.height));
+    });
     
+    if($.browser.touchDevice) {
+      $('body').bind('touchstart', function(e) {
+        if(e.targetTouches.length == 1) e.preventDefault();
+      });
+    }
+  });
+  
+/* ------------------------------------------------------------------------ */
+
+  maia.KaleidoscopeEditor = KaleidoscopeEditor;
+  function KaleidoscopeEditor(kaleidoscope) {
+    var $container = $('.editor');
+    $container.append(kaleidoscope.getLeafCanvas());
+    $('.editor input').bind('keypress', function(key) {
+      if(key.which == 13) {
+        kaleidoscope.setImgUrl($(this).val());
+      }
+    });
   }
 })();
